c3f4a125dcd9a8f0ffa762b4ae03efe34a74a9e5
MattBanuag <matthewbanuag.dev@gmail.com>
2022-12-22 10:28:47 -0600

    first commit

---
+++ assets/scripts/index.js
@@ -0,0 +1,10 @@
+'use strict';
+// UTILITIES & FUNCTIONS
+// Add Event Listener Function
+function onEvent(event, selector, callback) {
+    return selector.addEventListener(event, callback);
+}
+// Query Selector Function
+function select(selector, parent = document) {
+    return parent.querySelector(selector);
+}
